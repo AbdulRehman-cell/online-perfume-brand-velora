6e413bbf25f79b59b1a2cfc10e17da5f9c82a64c
AbdulRehman-cell <abdulrehmanishaque32@gmail.com>
2025-08-24 15:24:05 +0500

    fix

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 const session = require("express-session");
-import bodyParser from "body-parser";
+const bodyParser = require("body-parser");
 
 
 
